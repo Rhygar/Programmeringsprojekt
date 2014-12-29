35786af64ca4494afaab9fa614a62295ab990a95
taurus84 <David@Davids-iMac.local>
2014-12-29 15:09:53 +0100

    Change3

--- src/arrayTools/TestArrays.java
+++ src/arrayTools/TestArrays.java
@@ -6,10 +6,10 @@ public class TestArrays {
 	
 	public static void main(String[] args) throws Exception {
 
-		Array7x7 arr77 = new Array7x7();				//test constructor Array7x7
+//		Array7x7 arr77 = new Array7x7();				//test constructor Array7x7
 		TestEnvironment testEnv = new TestEnvironment();
-		int[] arr = {1,2,3,4,5,6,7};
-		Array7 arr7 = new Array7(arr);					//test constructor Array7
+//		int[] arr = {1,2,3,4,5,6,7};
+//		Array7 arr7 = new Array7(arr);					//test constructor Array7
 //		testEnv.setArray7x7(arr77);
 //		testEnv.setVerticalArray(arr7.getArray());			//test getArray() in Array7
 //		testEnv.setHorizontalArray(arr7.getArray());			
@@ -32,7 +32,7 @@ public class TestArrays {
 //		testEnv.setArray7x7(array2);
 //		
 		ArrayChars chars = new ArrayChars();
-		Array7x7 array3 = chars.getChar('A');
+//		Array7x7 array3 = chars.getChar('A');
 		testEnv.setArray7x7(chars.getChar('B'));
 		
 		
